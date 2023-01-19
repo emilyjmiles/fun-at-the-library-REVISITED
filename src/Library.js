51e415e8af6f21591954c7511c8ae401c9a75ba2
Emily Miles <ms.emily.j.miles@gmail.com>
2023-01-19 11:55:35 -0500

    Add implementation code to pass all tests

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,34 @@
+const createLibrary = (library) => {
+  return {
+    name: library,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  };
+};
 
+const addBook = (library, book) => {
+  return library.shelves[book.genre].push(book);
+};
+
+const checkoutBook = (library, title, genre) => {
+  // I think this is probably the best way to solve this but I may want to revisit it to see if I can dry it up since I have the same return in two separate spots
+  if (library.shelves[genre].length) {
+    for (let i = 0; i < library.shelves[genre].length; i++) {
+      if (library.shelves[genre][i].title === title) {
+        library.shelves[genre].splice(i, 1);
+        return `You have now checked out ${title} from the ${library.name}`;
+      }
+      return `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
+    }
+  }
+  return `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
+};
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
--- test/Library-test.js
+++ test/Library-test.js
@@ -4,33 +4,33 @@ var {
   createLibrary,
   addBook,
   checkoutBook
-} = require("../src/library.js")
+} = require("../src/library.js");
 
-describe("library.js", function() {
-  describe("createLibrary", function() {
-    it.skip("should be a function", function() {
+describe("library.js", function () {
+  describe("createLibrary", function () {
+    it("should be a function", function () {
       assert.isFunction(createLibrary);
     });
 
-    it.skip("should have a name", function() {
+    it("should have a name", function () {
       var denverLibrary = createLibrary("Denver Public Library");
 
-    assert.equal(denverLibrary.name, "Denver Public Library");
+      assert.equal(denverLibrary.name, "Denver Public Library");
     });
 
-    it.skip("should be able to have a different name", function() {
+    it("should be able to have a different name", function () {
       var goldenLibrary = createLibrary("Golden Public Library");
 
       assert.equal(goldenLibrary.name, "Golden Public Library");
     });
 
-    it.skip("should have shelves", function() {
+    it("should have shelves", function () {
       var denverLibrary = createLibrary("Denver Public Library");
 
       assert.isObject(denverLibrary.shelves);
     });
 
-    it.skip("should have several different types of shelves", function() {
+    it("should have several different types of shelves", function () {
       var denverLibrary = createLibrary("Denver Public Library");
 
       assert.deepEqual(denverLibrary.shelves.fantasy, []);
@@ -39,35 +39,35 @@ describe("library.js", function() {
     });
   });
 
-  describe("addBook", function() {
-    it.skip("should add book to the fantasy shelf", function() {
+  describe("addBook", function () {
+    it("should add book to the fantasy shelf", function () {
       var denverLibrary = createLibrary("Denver Public Library");
       var dracula = {
         title: "Dracula",
         mainCharacter: { name: "Count Dracula", age: undefined, pronouns: "he/him" },
         pageCount: 418,
         genre: "fantasy"
-      }
+      };
 
       addBook(denverLibrary, dracula);
 
       assert.equal(denverLibrary.shelves.fantasy[0], dracula);
     });
 
-    it.skip("should add books to the correct shelves automatically", function() {
+    it("should add books to the correct shelves automatically", function () {
       var denverLibrary = createLibrary("Denver Public Library");
       var dracula = {
         title: "Dracula",
         mainCharacter: { name: "Count Draula", age: undefined, pronouns: "he/him" },
         pageCount: 418,
         genre: "fantasy"
-      }
+      };
       var paleBlueDot = {
         title: "The Pale Blue Dot",
         mainCharacter: undefined,
         pageCount: 187,
         genre: 'nonFiction'
-      }
+      };
 
       addBook(denverLibrary, dracula);
       addBook(denverLibrary, paleBlueDot);
@@ -77,26 +77,26 @@ describe("library.js", function() {
     });
   });
 
-  describe("checkoutBook", function() {
-    it.skip("should unshelf a book to check out a book to a patron", function() {
+  describe("checkoutBook", function () {
+    it("should unshelf a book to check out a book to a patron", function () {
       var dracula = {
         title: "Dracula",
         mainCharacter: { name: "Count Dracula", age: undefined, pronouns: "he/him" },
         pageCount: 418,
         genre: "fantasy"
-      }
+      };
       var bornACrime = {
         title: "Born a Crime",
         mainCharacter: { name: "Trevor Noah", age: 36, pronouns: "he/him" },
         pageCount: 304,
         genre: "nonFiction"
-      }
+      };
       var prideAndPrejudice = {
         title: "Pride and Prejudice",
         mainCharacter: { name: "Eliabeth Bennet", age: 20, pronouns: "she/her" },
         pageCount: 432,
         genre: "fiction"
-      }
+      };
       var denverLibrary = createLibrary("Denver Public Library");
 
       addBook(denverLibrary, dracula);
@@ -105,16 +105,16 @@ describe("library.js", function() {
 
       var result1 = checkoutBook(denverLibrary, "Pride and Prejudice", "fiction");
 
-      assert.deepEqual(denverLibrary.shelves, {fantasy: [dracula], fiction: [], nonFiction: [bornACrime]});;
+      assert.deepEqual(denverLibrary.shelves, { fantasy: [dracula], fiction: [], nonFiction: [bornACrime] });;
       assert.equal(result1, "You have now checked out Pride and Prejudice from the Denver Public Library");
 
       var result2 = checkoutBook(denverLibrary, "Born a Crime", "nonFiction");
 
-      assert.deepEqual(denverLibrary.shelves, {fantasy: [dracula], fiction: [], nonFiction: []});;
-      assert.equal(result2, "You have now checked out Born a Crime from the Denver Public Library")
+      assert.deepEqual(denverLibrary.shelves, { fantasy: [dracula], fiction: [], nonFiction: [] });;
+      assert.equal(result2, "You have now checked out Born a Crime from the Denver Public Library");
     });
 
-    it.skip("should only checkout a book if the book is on the shelves", function() {
+    it("should only checkout a book if the book is on the shelves", function () {
       var denverLibrary = createLibrary("Denver Public Library");
 
       var error1 = checkoutBook(denverLibrary, "The Fifth Season", "fantasy");
